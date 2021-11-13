b88125b57de47f9a615fae0bf6946ee3ac7799aa
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-13 14:03:00 +0100

    basename updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ const error404 = "Error 404 : Page not found"
 
 const Routing = () => {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/Playground'>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
