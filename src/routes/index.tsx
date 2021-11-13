5bedf9e99e7f95092f20670d068b89d4d5657645
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-13 14:00:59 +0100

    adding basename to routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ const error404 = "Error 404 : Page not found"
 
 const Routing = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
