35272bccb6a3fbf712b090f66b9a23e8dadecb01
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-13 07:46:47 +0100

    tester

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,5 @@
 import { Main } from '../styled'
-import {InfoCard} from '../../components/'
+import { InfoCard } from '../../components/'
 
 
 const About = () => {
@@ -7,7 +7,6 @@ const About = () => {
         <Main>
             <h2>About</h2>
             <InfoCard header="Have to think about this !!!">
-
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit corrupti dolorem ipsam quas reiciendis neque earum reprehenderit nulla sunt cupiditate ducimus deserunt libero tempore magnam quasi sed, laboriosam possimus corporis molestiae nostrum pariatur alias praesentium. Accusantium, ipsa quo deserunt libero esse numquam molestiae qui totam perferendis illum cum. Atque blanditiis, laudantium quos nostrum consequatur iure fugit eveniet architecto quisquam quod ex esse ab accusantium veniam cupiditate illum iusto rerum facere at consequuntur explicabo natus mollitia ducimus? Omnis in deserunt perspiciatis similique quisquam, rerum consequuntur accusantium facilis, qui amet culpa, cupiditate perferendis eius veritatis! Asperiores tempore dolor, voluptates accusantium quas quaerat?
             </InfoCard>
             <h3 style={{ marginTop: "40px" }}>What was used?</h3>
@@ -19,7 +18,7 @@ const About = () => {
                             React Typescript
                         </td>
                         <td>v 17.0.2 / v 4.1.2</td>
-                        <td style={{textAlign:"right"}}>
+                        <td style={{ textAlign: "right" }}>
                             <a href=" https://create-react-app.dev/" rel="noreferrer" target="_blank">link</a>
                         </td>
                     </tr>
@@ -30,7 +29,7 @@ const About = () => {
                             React Router
                         </td>
                         <td>v 6.0.2</td>
-                        <td style={{textAlign:"right"}}>
+                        <td style={{ textAlign: "right" }}>
                             <a href="https://reactrouter.com/" rel="noreferrer" target="_blank">link</a>
                         </td>
                     </tr>
@@ -40,7 +39,7 @@ const About = () => {
                             Styled-Components
                         </td>
                         <td>v 5.3.3</td>
-                        <td style={{textAlign:"right"}}>
+                        <td style={{ textAlign: "right" }}>
                             <a href="https://styled-components.com/" rel="noreferrer" target="_blank">link</a>
                         </td>
                     </tr>
