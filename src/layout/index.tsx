c504e474c3000216fb59442dca8bbc8c4c569c2d
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-22 10:37:21 +0100

    fixed width of MainContent

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout = () => {
           <MenuLink to="/about">About</MenuLink>
         </nav>
       </Header>
-      <MainContent maxWidth={1000}>
+      <MainContent maxWidth={800}>
         <Outlet />
       </MainContent>
       <Footer>
