b335271c012fc8eb0825ef23d9ef2f9fc1b17ff4
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-23 02:56:57 +0100

    Fixed Scrollbars in Main

--- src/pages/styled.ts
+++ src/pages/styled.ts
@@ -6,9 +6,8 @@ export const Main = styled.main`
     background-color: white;
     height: calc(100vh - 170px); // 90 + 48 + 16 + 16 = 170 (( Header + Footer + MarginTop + MarginBottom ))
     margin: 1rem 0;
-    /* box-shadow: rgba(0, 0, 0, 0.3) 0px 5px 10px 0px inset,rgba(0, 0, 0, 0.3) 0px -5px 10px 0px inset;  */
     overflow-y: auto;
-    border-radius: 12px;
+    border-radius: 0.5rem;
 
     // Firefox scrollbar support
     scrollbar-color: ${({ theme }) => theme.COLOR.PRIMARY} transparent;
@@ -16,10 +15,11 @@ export const Main = styled.main`
 
     // Webkit scrollbar support
     &::-webkit-scrollbar {
-      width: 0.5rem;
-      background-color: transparent;
+      width: 1rem;
     }
     &::-webkit-scrollbar-thumb {
       background: linear-gradient(${({ theme }) => theme.COLOR.PRIMARY}, rgba(18, 52, 86, 0.5));
+      border-radius: 0.5rem;
+      border: 2px solid white;
     }
 `;
