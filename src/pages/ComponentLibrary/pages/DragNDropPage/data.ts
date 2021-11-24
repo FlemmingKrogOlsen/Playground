3d2a9b1e8bd54bab14a58d4f689a6c982baeed4a
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-24 17:03:31 +0100

    DragNDrop Page cleaned up

---
+++ src/pages/ComponentLibrary/pages/DragNDropPage/data.ts
@@ -0,0 +1,39 @@
+export const initial_state = [
+    {
+      id: 1,
+      title: "Title Group #1",
+      items: [
+        {
+          itemId: 1,
+          itemContent: "<b>#1_1</b>",
+        },
+        {
+          itemId: 2,
+          itemContent: "<b>#1_2</b>",
+        },
+        {
+          itemId: 3,
+          itemContent: "<b>#1_3</b>",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Title Group #2",
+      items: [
+        {
+          itemId: 4,
+          itemContent: "<b>#2_1</b>",
+        },
+        {
+          itemId: 5,
+          itemContent: "<b>#2_2</b>",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Title Group #3",
+      items: [],
+    },
+  ];
--- src/pages/ComponentLibrary/pages/DragNDropPage/index.tsx
+++ src/pages/ComponentLibrary/pages/DragNDropPage/index.tsx
@@ -1,52 +1,19 @@
 import { useState } from "react";
 import { DragNDrop } from "../../../../components";
-
-const initial_state = [
-  {
-    id: 1,
-    title: "Title Group #1",
-    items: [
-      {
-        itemId: 1,
-        itemContent: "<b>#1_1</b>",
-      },
-      {
-        itemId: 2,
-        itemContent: "<b>#1_2</b>",
-      },
-      {
-        itemId: 3,
-        itemContent: "<b>#1_3</b>",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Title Group #2",
-    items: [
-      {
-        itemId: 4,
-        itemContent: "<b>#2_1</b>",
-      },
-      {
-        itemId: 5,
-        itemContent: "<b>#2_2</b>",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Title Group #3",
-    items: [],
-  },
-];
+import { Editor, CodeBlock } from "../styled";
+import { initial_state } from "./data";
 
 const DragNDropPage = () => {
   const [data, setData] = useState(initial_state);
   return (
     <>
       <DragNDrop list={data} onListChange={setData} groupWidth={200} />
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Editor>
+        <CodeBlock>
+        <pre style={{fontSize: "10px"}}>{JSON.stringify(data, null, 2)}</pre>
+        </CodeBlock>
+      </Editor>
+      
     </>
   );
 };
