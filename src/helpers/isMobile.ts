acedc37cca4cdb9a4d31025885ec4fca27addb58
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-11-30 22:33:58 +0100

    isMobile simplyfied

--- src/helpers/isMobile.ts
+++ src/helpers/isMobile.ts
@@ -1,16 +1,10 @@
-import isBrowser from "./isBrowser"
+import isBrowser from "./isBrowser";
 
 const isMobile = (): boolean => {
-  if (isBrowser) {
-    if (window.matchMedia("(max-width: 600px)").matches) {
-      // window width is at less than 600px
-      return true
-    } else {
-      // window width is greater than 600px
-      return false
-    }
+  if (!isBrowser) {
+    return false;
   }
-  return false
-}
+  return window.matchMedia("(max-width: 600px)").matches;
+};
 
-export default isMobile
+export default isMobile;
