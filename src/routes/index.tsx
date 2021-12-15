ce96c864ac38ed1b6ebc689e0f970d2ab220d432
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-12-15 06:27:59 +0100

    refactored routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Layout from '../layout'
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Components, { ComponentsMainPage } from '../pages/ComponentLibrary';
-import PageTemplate from '../pages/ComponentLibrary/pageTemplate';
+import Layout from "layout";
+import Error from "./error";
 
-import Error from './error'
+import Home from "pages/Home";
+import About from "pages/About";
+import Components, { ComponentsMainPage } from "pages/ComponentLibrary";
+import PageTemplate from "pages/ComponentLibrary/pageTemplate";
 
-const Routing = () => {
-  return (
-    <BrowserRouter basename='/Playground'>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/components" element={<Components />}>
-            <Route index element={<ComponentsMainPage />} />
-            <Route path=":componentId" element={<PageTemplate />} />
-          </Route>
-          <Route path="*" element={<Error />} />
+const Routing = () => (
+  <BrowserRouter basename="/Playground">
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/components" element={<Components />}>
+          <Route index element={<ComponentsMainPage />} />
+          <Route path=":componentId" element={<PageTemplate />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
-export default Routing
+export default Routing;
