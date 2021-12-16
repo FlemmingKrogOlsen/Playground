7f9fd319f6837c410385c02f59fe448e5fdeef83
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-12-16 01:20:10 +0100

    Kinda fixed magic number

--- src/layout/styled.ts
+++ src/layout/styled.ts
@@ -1,6 +1,13 @@
 import { NavLink, Link } from "react-router-dom";
 import styled, { css } from "styled-components";
 
+const headerHeight: number = 96;
+const footerHeight: number = 48;
+const contentMarginTopBottom: number = 16;
+
+const reduceSize: number =
+  headerHeight + footerHeight + 2 * contentMarginTopBottom;
+
 const bars = css`
   &::before,
   &::after {
@@ -22,6 +29,27 @@ const bars = css`
   }
 `;
 
+const scrollBarsCSS = css`
+  overflow-y: auto;
+
+  // Firefox scrollbar support
+  scrollbar-color: ${({ theme }) => theme.COLOR.PRIMARY} transparent;
+  scrollbar-width: thin;
+
+  // Webkit scrollbar support
+  &::-webkit-scrollbar {
+    width: 1rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: linear-gradient(
+      ${({ theme }) => theme.COLOR.PRIMARY},
+      rgba(18, 52, 86, 0.5)
+    );
+    border-radius: 0.5rem;
+    border: 2px solid ${({ theme }) => theme.COLOR.BACKGROUND};
+  }
+`;
+
 export const Header = styled.header`
   ${bars};
   position: relative;
@@ -30,6 +58,7 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
+  height: ${headerHeight}px;
 `;
 
 export const BrandLink = styled(Link)`
@@ -64,31 +93,19 @@ export const Footer = styled.footer`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  height: ${footerHeight}px;
   a {
     color: ${({ theme }) => theme.COLOR.TEXT};
   }
 `;
 
 export const Main = styled.main`
-    padding: 1rem;
-    width: 100%;
-    background-color: ${props => props.color ? props.color : props.theme.COLOR.BACKGROUND};
-    height: calc(100vh - 170px); // 90 + 48 + 16 + 16 = 170 (( Header + Footer + MarginTop + MarginBottom ))
-    margin: 1rem 0;
-    overflow-y: auto;
-    border-radius: 0.5rem;
-
-    // Firefox scrollbar support
-    scrollbar-color: ${({ theme }) => theme.COLOR.PRIMARY} transparent;
-    scrollbar-width: thin;
-
-    // Webkit scrollbar support
-    &::-webkit-scrollbar {
-      width: 1rem;
-    }
-    &::-webkit-scrollbar-thumb {
-      background: linear-gradient(${({ theme }) => theme.COLOR.PRIMARY}, rgba(18, 52, 86, 0.5));
-      border-radius: 0.5rem;
-      border: 2px solid ${({ theme }) => theme.COLOR.BACKGROUND};
-    }
+  padding: ${contentMarginTopBottom}px;
+  width: 100%;
+  background-color: ${(props) =>
+    props.color ? props.color : props.theme.COLOR.BACKGROUND};
+  height: calc(100vh - ${reduceSize}px);
+  margin: 1rem 0;
+  border-radius: 0.5rem;
+  ${scrollBarsCSS};
 `;
