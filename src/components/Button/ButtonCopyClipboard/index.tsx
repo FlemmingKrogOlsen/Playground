b2666bd6cd3d0060679f26c772b934f05776588c
Flemming Krog Olsen <flemming@krogolsen.dk>
2022-01-22 06:21:17 +0100

    Updated CopytoClipboard

--- src/components/Button/ButtonCopyClipboard/index.tsx
+++ src/components/Button/ButtonCopyClipboard/index.tsx
@@ -1,27 +1,36 @@
 import Button from "components/Button";
+import { useState } from "react";
 
-const ButtonCopyClipboard = ({ text }: { text: string }) => (
-  <Button
-    onClick={() => {
-      navigator.clipboard.writeText(text);
-    }}
-    title="Copy2Clipboard"
-    icon={
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-        />
-      </svg>
-    }
-  />
-);
+const ButtonCopyClipboard = ({ text }: { text: string }) => {
+  const [copied, setCopied] = useState(false);
+  return (
+    <Button
+      onClick={() => {
+        navigator.clipboard.writeText(text);
+        setCopied(true);
+      }}
+      title={copied ? "Copied !" : "Copy to Clipboard"}
+      icon={
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d={
+              copied
+              ? "M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+              : "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+            }
+          />
+        </svg>
+      }
+    />
+  );
+};
 
 export default ButtonCopyClipboard;
